4171b6f52f59e7d589078ba7f85a324c7832af04
Kramer <kramermp5@gmail.com>
2021-02-18 17:27:38 -0500

    Added Random Color Theme

---
+++ src/main/java/binavigator/ui/colortheme/RandomColorTheme.java
@@ -0,0 +1,32 @@
+package binavigator.ui.colortheme;
+
+import java.awt.*;
+import java.util.Random;
+
+public class RandomColorTheme extends TextColorTheme{
+	private static Random rng = new Random();
+
+	private static Color getRandomColor() {
+		return (new Color(rng.nextInt(255), rng.nextInt(255), rng.nextInt(255)));
+	}
+
+	@Override
+	public Color getTextColor() {
+		return getRandomColor();
+	}
+
+	@Override
+	public Color getCommentColor() {
+		return getRandomColor();
+	}
+
+	@Override
+	public Color getStringColor() {
+		return getRandomColor();
+	}
+
+	@Override
+	public Color getKeyWordColor() {
+		return getRandomColor();
+	}
+}
