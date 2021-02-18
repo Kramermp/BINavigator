1ca664c49165b6789a8161076427e12d50f9c94b
Kramer <kramermp5@gmail.com>
2021-02-18 17:15:00 -0500

    Added Color Theme Class

---
+++ src/main/java/binavigator/ui/colortheme/ColorTheme.java
@@ -0,0 +1,13 @@
+package binavigator.ui.colortheme;
+
+import java.awt.*;
+
+public abstract class ColorTheme {
+	public abstract Color getTextColor();
+	public abstract Color getCommentColor();
+	public abstract Color getStringColor();
+	public abstract Color getKeyWordColor();
+
+
+
+}
---
+++ src/main/java/binavigator/ui/colortheme/Monokai.java
@@ -0,0 +1,36 @@
+package binavigator.ui.colortheme;
+
+import java.awt.*;
+
+public class Monokai extends ColorTheme{
+	private static final Color WHITE = Color.decode("#F8F8F2");
+	private static final Color RED = Color.decode("#F92672");
+	private static final Color DARK_GRAY = Color.decode("#75715E");
+	private static final Color ORANGE = Color.decode("#FD971F");
+	private static final Color Light_Orange = Color.decode("#E69F66");
+	private static final Color YELLOW = Color.decode("#E6DB74");
+	private static final Color GREEN = Color.decode("#A6E22E");
+	private static final Color Blue = Color.decode("#66D9EF");
+	private static final Color Purple = Color.decode("#AE81FF");
+
+
+	@Override
+	public Color getTextColor() {
+		return this.WHITE;
+	}
+
+	@Override
+	public Color getCommentColor() {
+		return this.DARK_GRAY;
+	}
+
+	@Override
+	public Color getStringColor() {
+		return this.YELLOW;
+	}
+
+	@Override
+	public Color getKeyWordColor() {
+		return this.RED;
+	}
+}
