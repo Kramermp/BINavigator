2c9c29a93adcf851901ab631f6cc174a3d32b63c
Kramer <kramermp5@gmail.com>
2021-02-19 22:38:49 -0500

    Added some keywords for highlighting

--- src/main/java/binavigator/backend/sql/SqlHelper.java
+++ src/main/java/binavigator/backend/sql/SqlHelper.java
@@ -5,10 +5,22 @@ import binavigator.backend.ArrayUtils;
 import java.util.Arrays;
 
 public class SqlHelper {
-	public static final String[] keyWordArray = new String[]{"SELECT", "FROM"};
+
+	public static final String[] keyWordArray = new String[]{"SELECT", "FROM", "CREATE", "INSERT", "INTO", "ALTER",
+			"ADD", "DISTINCT", "UNIQUE", "UPDATE", "SET", "DELETE", "TRUNCATE", "AS", "ORDER", "BY", "ASC", "DESC",
+			"BETWEEN", "OR", "NOT", "LIMIT", "IS", "NULL", "DROP", "COLUMN", "TABLE", "DATABASE", "GROUP", "HAVING",
+			"IN", "JOIN", "UNION", "EXISTS", "LIKE", "CASE", "WHERE"};
 
 	public static boolean isKeyWord(String toCheck) {
 		return ArrayUtils.containsIgnoreCase(keyWordArray, toCheck);
+
+	}
+
+	public static void parseSql(String sqlToParse) {
+		StringBuilder build = new StringBuilder(sqlToParse);
+
+		//Check for Semi Colons to split statements
+
 	}
 
 
