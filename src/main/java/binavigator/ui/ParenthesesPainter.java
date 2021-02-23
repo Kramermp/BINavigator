4902fa0977a8b82cbf4d21d29ded15c71355fdc4
Kramer <kramermp5@gmail.com>
2021-02-23 01:06:42 -0500

    Added Ability to paint between specific characters

--- src/main/java/binavigator/ui/ParenthesesPainter.java
+++ src/main/java/binavigator/ui/ParenthesesPainter.java
@@ -22,6 +22,10 @@ public class ParenthesesPainter implements Highlighter.HighlightPainter, CaretLi
 
 	private Rectangle lastView;
 
+	private Rectangle[] lastShape = new Rectangle[]{};
+
+	private Rectangle[] currentShapes = new Rectangle[]{};
+
 	/*
 	 *  The line color will be calculated automatically by attempting
 	 *  to make the current selection lighter by a factor of 1.2.
@@ -60,7 +64,7 @@ public class ParenthesesPainter implements Highlighter.HighlightPainter, CaretLi
 			catch(BadLocationException ble) {}
 		}
 
-	public Rectangle getCurrentView(JTextComponent component) {
+	public Rectangle getCurrentView(JTextComponent component) throws BadLocationException {
 		Rectangle rec = null;
 		rec = new Rectangle();
 
@@ -85,8 +89,10 @@ public class ParenthesesPainter implements Highlighter.HighlightPainter, CaretLi
 			rec.height = rec.height * lineCount;
 		} else  if (checkForCloseParenthese(component)){
 			rec.width = component.getWidth();
-			rec.x = component.getMargin().left;
+
 			FontMetrics fm = component.getGraphics().getFontMetrics(parentController.getFont());
+			String leadingString = component.getText().substring(Utilities.getRowStart(component, component.getCaretPosition()), component.getCaretPosition());
+			rec.x = component.getMargin().left + fm.stringWidth(leadingString);
 			rec.y  = (fm.getHeight() * (component.getText().substring(0, component.getCaretPosition()).split(("\n")).length)) + component.getMargin().top  ;
 			rec.height = fm.getHeight();
 
@@ -122,13 +128,111 @@ public class ParenthesesPainter implements Highlighter.HighlightPainter, CaretLi
 	}
 
 
+	public void updateCharacterShape(JTextComponent component, int beginIndex, int endIndex) throws BadLocationException {
+		lastShape = currentShapes;
+		System.out.println("Get from character " + beginIndex + " - " + endIndex);
+		if(beginIndex  == endIndex) {
+//			return new Polygon();
+		}
+
+		String[] lines = component.getText().substring(beginIndex, endIndex).split("\n", - 1);
+
+		switch (lines.length) {
+			case 0:
+				currentShapes = new Rectangle[] {};
+				return;
+			case 1:
+				currentShapes =  new Rectangle[]{getSingleLineShape(component, beginIndex, endIndex)};
+				return;
+			default:
+				currentShapes = getMultiLineShape(component, beginIndex, endIndex, lines.length);
+		}
+	}
+
+	private Rectangle getSingleLineShape(JTextComponent component, int beginIndex, int endIndex) {
+		System.out.println("Painting Single Line");
+		Rectangle rec = null;
+
+		try {
+			FontMetrics fm = getFontMetric(component);
+			rec = new Rectangle();
+
+			//X Position = Margin + size of SubString from line start to Begin
+			rec.x = component.getMargin().left + fm.stringWidth(component.getText().substring(Utilities.getRowStart(component, beginIndex), beginIndex));
+			rec.width = fm.stringWidth(component.getText().substring(beginIndex, endIndex));
+
+			rec.y = component.getMargin().top + (component.getText().substring(0, beginIndex).split("\n", -1).length - 1 )* fm.getHeight();
+			rec.height = fm.getHeight();
+
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+		}
+
+		return rec;
+	}
+
+	private Rectangle[] getMultiLineShape (JTextComponent component, int beginIndex, int endIndex, int lineCount) throws BadLocationException {
+		Rectangle[] recs = new Rectangle[lineCount];
+		FontMetrics fm = getFontMetric(component);
+
+		if(lineCount == 0) {
+			return new Rectangle[]{};
+		} else if (lineCount == 1) {
+			return new Rectangle[] {getSingleLineShape(component, beginIndex, endIndex)};
+		} else {
+			int lineOffset = (component.getText().substring(0, beginIndex).split("\n", -1).length - 1 );
+
+			// First And Last Line have to Be handled specially
+			recs[0] = new Rectangle();
+			recs[0].x = component.getMargin().left + fm.stringWidth(component.getText().substring(Utilities.getRowStart(component, beginIndex), beginIndex));
+			recs[0].width = component.getWidth();
+
+			recs[0].y = component.getMargin().top + lineOffset * fm.getHeight();
+			recs[0].height = fm.getHeight();
+
+			for(int i = 1; i < recs.length - 2; i++) {
+				recs[i].x = component.getMargin().left;
+				recs[i].width = component.getWidth();
+
+				recs[i].y = component.getMargin().top + ((lineOffset + i) * fm.getHeight());
+				recs[i].height = fm.getHeight();
+			}
+
+			recs[recs.length - 1] = new Rectangle();
+
+			recs[recs.length - 1].x = component.getMargin().left;
+			recs[recs.length - 1].width = component.getMargin().left + fm.stringWidth(component.getText().substring(Utilities.getRowStart(component, beginIndex), beginIndex));
+
+			recs[recs.length - 1].y = component.getMargin().top + ((lineOffset + recs.length - 1) * fm.getHeight());
+			recs[recs.length - 1].height = fm.getHeight();
+		}
+
+		return recs;
+	}
+
+	private FontMetrics getFontMetric(JTextComponent textComponent) {
+		return  textComponent.getGraphics().getFontMetrics(component.getFont());
+	}
+
+
 	//  Paint the background highlight
 
 		public void paint(Graphics g, int p0, int p1, Shape bounds, JTextComponent c)
 		{
+			Rectangle rec = null;
 
-			Rectangle rec = rec = getCurrentView(c);
-				if(checkForOpenParentheses(c)) {
+			g.setColor(Color.GREEN);
+			for(int i = 0; i < this.currentShapes.length; i++) {
+				g.fillRect(currentShapes[i].x, currentShapes[i].y, currentShapes[i].width, currentShapes[i].height);
+			}
+
+			try {
+				rec = getCurrentView(c);
+			} catch (BadLocationException e) {
+				e.printStackTrace();
+			}
+
+			if(checkForOpenParentheses(c)) {
 					System.out.println("Sittting on opening parentheses");
 					g.setColor(parentController.getParenthesePaintColor());
 
@@ -164,7 +268,29 @@ public class ParenthesesPainter implements Highlighter.HighlightPainter, CaretLi
 				public void run()
 				{
 					System.out.println("Reseting Highlight");
-					Rectangle currentView = getCurrentView(component);
+
+
+					try {
+						updateCharacterShape(component, 10, 20);
+					} catch (BadLocationException e) {
+						e.printStackTrace();
+					}
+					lastShape = new Rectangle[]{new Rectangle()};
+					if(lastShape.length == 0 || currentShapes.length == 0 || lastShape[0].x !=  currentShapes[0].x || currentShapes[0].y != lastShape[0].y) {
+						for(int i = 0; i < currentShapes.length; i++) {
+							component.paintImmediately(currentShapes[i]);
+						}
+						for(int i = 0; i < lastShape.length; i++) {
+							component.paintImmediately(lastShape[i]);
+						}
+					}
+
+					Rectangle currentView = null;
+					try {
+						currentView = getCurrentView(component);
+					} catch (BadLocationException e) {
+						e.printStackTrace();
+					}
 
 					//  Remove the highlighting from the previously highlighted line
 					if (!lastView.equals(currentView)) {
@@ -180,6 +306,14 @@ public class ParenthesesPainter implements Highlighter.HighlightPainter, CaretLi
 					}
 				}
 			});
+		}
+
+		public void getOpenIndex(JTextComponent component) {
+
+		}
+
+		public void getCloseIndex(JTextComponent component) {
+
 		}
 
 		//  Implement CaretListener
