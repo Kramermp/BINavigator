b77637f29dbfce6bf01b39835ef5bca367cd21a7
Kramer <kramermp5@gmail.com>
2021-02-26 23:13:34 -0500

    Reformatted code to be inline with other code style

--- src/main/java/binavigator/ui/texteditor/CaretLinePainter.java
+++ src/main/java/binavigator/ui/texteditor/CaretLinePainter.java
@@ -37,8 +37,7 @@ public class CaretLinePainter implements Highlighter.HighlightPainter {
 	 *  @param component  text component that requires background line painting
 	 *  @param color      the color of the background line
 	 */
-	public CaretLinePainter(JTextComponent component, Color color)
-	{
+	public CaretLinePainter(JTextComponent component, Color color) {
 		this.component = component;
 		setColor( color );
 
@@ -46,8 +45,7 @@ public class CaretLinePainter implements Highlighter.HighlightPainter {
 
 		//  Turn highlighting on by adding a dummy highlight
 
-		try
-		{
+		try {
 			component.getHighlighter().addHighlight(0, 0, this);
 		}
 		catch(BadLocationException ble) {}
@@ -58,8 +56,7 @@ public class CaretLinePainter implements Highlighter.HighlightPainter {
 	 *
 	 *  @param color  the color of the background line
 	 */
-	public void setColor(Color color)
-	{
+	public void setColor(Color color) {
 		this.color = color;
 	}
 
@@ -68,8 +65,7 @@ public class CaretLinePainter implements Highlighter.HighlightPainter {
 	 *
 	 *  @return the color of the background line
 	 */
-	public void setLighter(Color color)
-	{
+	public void setLighter(Color color) {
 		int red   = Math.min(255, (int)(color.getRed() * 1.1));
 		int green = Math.min(255, (int)(color.getGreen() * 1.1));
 		int blue  = Math.min(255, (int)(color.getBlue() * 1.1));
@@ -78,22 +74,19 @@ public class CaretLinePainter implements Highlighter.HighlightPainter {
 
 	//  Paint the background highlight
 
-	public void paint(Graphics g, int p0, int p1, Shape bounds, JTextComponent c)
-	{
+	public void paint(Graphics g, int p0, int p1, Shape bounds, JTextComponent c) {
 
 	}
 
 	public void refreshActiveLine() {
-		try
-		{
+		try {
 			int offset =  component.getCaretPosition();
 			Rectangle currentView = component.modelToView(offset);
 
 			//  Remove the highlighting from the previously highlighted line
 
 			System.out.println(currentView.width);
-			if (lastView.y != currentView.y)
-			{
+			if (lastView.y != currentView.y) {
 				component.repaint(0, lastView.y, component.getWidth(), lastView.height);
 				component.repaint(0, currentView.y, component.getWidth(), currentView.height);
 				lastView = currentView;
@@ -103,8 +96,7 @@ public class CaretLinePainter implements Highlighter.HighlightPainter {
 	}
 
 	public void fillActiveLineShape(Graphics g) {
-		try
-		{
+		try {
 			Rectangle r = component.modelToView(component.getCaretPosition());
 			g.setColor( color );
 			g.fillRect(component.getMargin().left, r.y, component.getWidth(), r.height);
